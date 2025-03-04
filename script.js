63785e3a94eb421c16552a495959fb7a9979aae0
daeekim <asdxl1030@naver.com>
2025-03-05 01:40:08 +0900

    표시 문구 변경

--- script.js
+++ script.js
@@ -300,7 +300,7 @@ class SeatAssignment {
             
             this.seatNumberDisplay.textContent = `${this.userSeat.number}번입니다`;
         } else {
-            this.seatNumberDisplay.textContent = '좌석을 선택해주세요';
+            this.seatNumberDisplay.textContent = '성별 선택 후 선택을 눌러주세요';
         }
     }
     
